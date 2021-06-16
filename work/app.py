4798dcc352f4ee4e5adc925a10461cbf3bed6bc3
dothehokiepokie <84648822+dothehokiepokie@users.noreply.github.com>
2021-06-16 14:12:28 -0700

    Add files via upload
    fixed a out of range error

--- work/app.py
+++ work/app.py
@@ -1,7 +1,7 @@
 import string
 
 # input string
-str1 = "Theres a lady whos sure all that glitters is gold And shes buying a stairway to Heaven When she gets there she knows, if the stores are all closed With a word she can get what she came for Ooh, ooh, and shes buying a stairway to Heaven Theres a sign on the wall, but she wants to be sure Cause you know sometimes words have two meanings In a tree by the brook, theres a songbird who sings Sometimes all of our thoughts are misgiven Ooh, makes me wonder Ooh, makes me wonder Theres a feeling I get when I look to the west And my spirit is crying for leaving In my thoughts I have seen rings of smoke through the trees And the voices of those who stand looking Ooh, it makes me wonder Ooh, really makes me wonder And its whispered that soon if we all call the tune Then the piper will lead us to reason And a new day will dawn for those who stand long And the forests will echo with laughter Oh-oh-oh-oh-woahhh If theres a bustle in your hedgerow, dont be alarmed, now Its just a spring clean for the May queen Yes, there are two paths you can go by, but in the long run Theres still time to change the road youre on And it makes me wonder Ohh, woah Your head is humming and it wont go, in case you dont know The pipers calling you to join him Dear lady, can you hear the wind blow? And did you know Your stairway lies on the whispering wind? And as we wind on down the road Our shadows taller than our soul There walks a lady we all know Who shines white light and wants to show How everything still turns to gold And if you listen very hard The tune will come to you at last When all are one and one is all To be a rock and not to roll And shes buying a stairway to Heaven"
+str1 = "The Lord word says way too bee"
 
 # convert string to lowercase
 str1 = str1.lower()
@@ -66,19 +66,30 @@ for letter in new_string:
 
         for word_letter in letter_string:
             if (word_letter in "aeiouy") & (vowel_1 > 1) & (consonant > 1) & (count_2 == 0):
-
-                if number_of_letters_in_the_word == short_sorted_unique_list[-1]:
-                    short_new_mix_string_short += letter_string
-                elif number_of_letters_in_the_word == short_sorted_unique_list[-2]:
-                    short_new_mix_string_short += letter_string
+                if len(short_sorted_unique_list) >= 2:
+                    if number_of_letters_in_the_word == short_sorted_unique_list[-1]:
+                        short_new_mix_string_short += letter_string
+                    elif number_of_letters_in_the_word == short_sorted_unique_list[-2]:
+                        short_new_mix_string_short += letter_string
                 count_2 += 1
 
             if (word_letter in "aeiouy") & (vowel_1 > 0) & (consonant > 0) & (count == 0):
 
-                if number_of_letters_in_the_word == long_sorted_unique_list[-3]:
-                    long_new_mix_string_long += letter_string
-                elif number_of_letters_in_the_word == long_sorted_unique_list[-4]:
-                    long_new_mix_string_long += letter_string
+                if len(long_sorted_unique_list) >= 4:
+                    if number_of_letters_in_the_word == long_sorted_unique_list[-3]:
+                        long_new_mix_string_long += letter_string
+                    elif number_of_letters_in_the_word == long_sorted_unique_list[-4]:
+                        long_new_mix_string_long += letter_string
+                elif len(long_sorted_unique_list) >= 3:
+                    if number_of_letters_in_the_word == long_sorted_unique_list[-2]:
+                        long_new_mix_string_long += letter_string
+                    elif number_of_letters_in_the_word == long_sorted_unique_list[-3]:
+                        long_new_mix_string_long += letter_string
+                elif len(long_sorted_unique_list) >= 2:
+                    if number_of_letters_in_the_word == long_sorted_unique_list[-1]:
+                        long_new_mix_string_long += letter_string
+                    elif number_of_letters_in_the_word == long_sorted_unique_list[-2]:
+                        long_new_mix_string_long += letter_string
                 count += 1
             if (word_letter in "bcdfghjklmnpqrstvwxz"):
                 consonant += 1
