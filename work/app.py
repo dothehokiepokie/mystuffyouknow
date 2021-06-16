59931a68361fc0ac17e0f679898071bb7f07b099
dothehokiepokie <84648822+dothehokiepokie@users.noreply.github.com>
2021-06-16 04:37:56 -0700

    Add files via upload
    tweaked the results so they are more fun to read and so therefore it means more in a meaningful way.

--- work/app.py
+++ work/app.py
@@ -1,7 +1,7 @@
 import string
 
 # input string
-str1 = "this finds shorter strings inside a longer unedited string to parse out a deeper hidden meaning in the original string."
+str1 = "Mr. Crowley, what went on in your head? Oh, Mr. Crowley, did you talk to the dead? Your life style to me, seemed so tragic With the thrill of it all You fooled all the people with magic Yeah, you waited on Satans call Mr. Charming, did you think you were pure? Mr. Alarming, in nocturnal rapport Uncovering things that were sacred Manifest on this Earth Conceived in the eye of a secret And they scattered the afterbirth Mr. Crowley, wont you ride my white horse? Mr. Crowley, its symbolic, of course Approaching a time that is classic I hear that maidens call Approaching a time that is drastic Standing with their backs to the wall Was it polemically sent? I wanna know what you meant I wanna know, I wanna know what you meant, yeah"
 
 # convert string to lowercase
 str1 = str1.lower()
@@ -67,17 +67,17 @@ for letter in new_string:
         for word_letter in letter_string:
             if (word_letter in "aeiouy") & (vowel_1 > 1) & (consonant > 1) & (count_2 == 0):
 
-                if number_of_letters_in_the_word == short_sorted_unique_list[0]:
+                if number_of_letters_in_the_word == short_sorted_unique_list[-1]:
                     short_new_mix_string_short += letter_string
-                elif number_of_letters_in_the_word == short_sorted_unique_list[1]:
+                elif number_of_letters_in_the_word == short_sorted_unique_list[-2]:
                     short_new_mix_string_short += letter_string
                 count_2 += 1
 
             if (word_letter in "aeiouy") & (vowel_1 > 0) & (consonant > 0) & (count == 0):
 
-                if number_of_letters_in_the_word == long_sorted_unique_list[0]:
+                if number_of_letters_in_the_word == long_sorted_unique_list[-1]:
                     long_new_mix_string_long += letter_string
-                elif number_of_letters_in_the_word == long_sorted_unique_list[1]:
+                elif number_of_letters_in_the_word == long_sorted_unique_list[-2]:
                     long_new_mix_string_long += letter_string
                 count += 1
             if (word_letter in "bcdfghjklmnpqrstvwxz"):
